b8904fc6cdc79ca9f5e16123a990942133718e1f
nurmodena <91444202+nurmodena@users.noreply.github.com>
2023-06-17 10:42:39 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const HEROES = [
+  { name: 'Ironman', color: 'red' },
+  { name: 'Spiderman', color: 'blue' },
+  { name: 'Thor', color: 'orange' },
+  { name: 'Hulk', color: 'green' },
+  { name: 'Black Widow', color: 'black' }
+];
+
+const SuperHerro = ({ name, color, hapus }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex', flexDirection: 'row',
+      alignItems: 'center', justifyContent: 'space-between'
+    }}>
+      <h2 style={{ color }}>{name}</h2>
+      <a href='#' onClick={hapus(name)} style={{
+        textDecoration: 'none',
+        fontSize: 20,
+        fontWeight: 'bold'
+      }}>Hapus</a>
+    </div>
+  );
+}
+
+const App = () => {
+  const [heroes, setHeroes] = useState(HEROES);
+
+  const hapus = (name) => () => {
+    setHeroes((current) => {
+      return current.filter(e => e.name != name);
+    })
+  }
+
+  const reset = () => {
+    setHeroes(HEROES);
+  }
+
+  return (
+    <div style={{ width: 600, margin: '0 auto' }}>
+      <h1>Daftar Super Hero</h1>
+      <hr />
+      {
+        heroes.map((item, i) =>
+        (<SuperHerro key={`key-${i}`} name={item.name}
+          color={item.color} hapus={hapus} />))
+      }
+      <hr />
+      <a href='#' onClick={reset} style={{
+        textDecoration: 'none', fontSize: 20, fontWeight: 'bold'
+      }}>Reset</a>
     </div>
   );
 }
